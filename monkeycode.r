783c761643e3969213f6599905e1ea296d4e3062
Gabriel Al-Ghalith <GabeAl@users.noreply.github.com>
2017-07-03 01:42:11 -0500

    Updates: Swarms, polyserial stats, and more!

--- monkeycode.r
+++ monkeycode.r
@@ -17,12 +17,20 @@ map$PA = factor(map$PA,levels=c("USA zoo","SE Asia zoo","EPRC","Wild"),ordered =
 map$Population = map$PA # overwrite the old Population column with the new
 
 #### PICRUST ####
+#install.packages("polycor")
+library(polycor)
+
 # Go through each picrust pathway and test for significance w/group
 CW = map$CaptiveWild %in% c("Captive","Wild") # For selecting only captive and wild
 Grp.Pvals = rep(1,nrow(picrust)) # Initialize p values for groupwise sig. tests
+Grp.Corrs = rep(0,nrow(picrust)) # Initialize correlation chamber (groupwise)
 Wld.Pvals = rep(1,nrow(picrust)) # Initialize p values for wild vs captive tests
 for (m.ix in 1:nrow(picrust)) {  # Loop through all the rows (picrust pathways)
-  Grp.Pvals[m.ix] = kruskal.test(picrust[m.ix,] ~ map$PA)$p.value
+  try({
+    ps = polyserial(picrust[m.ix,],map$PA,ML=T,std.err = T)
+    Grp.Corrs[m.ix] = ps$rho
+    Grp.Pvals[m.ix] = 1-pchisq(ps$chisq, ps$df) 
+    },silent=T)
   Wld.Pvals[m.ix] = wilcox.test(picrust[m.ix,CW] ~ map$CaptiveWild[CW])$p.value
 }
 
@@ -31,16 +39,17 @@ Sort.ix = order(Wld.Pvals)                 # Stores the order of them (like thir
 PicrustIDs = rownames(picrust)[Sort.ix]    # Keep a list of the IDs in order
 Grp.Qvals = p.adjust(Grp.Pvals[Sort.ix],method = "holm") # Adj. group P-values in order
 Wld.Qvals = p.adjust(Wld.Pvals[Sort.ix],method = "holm") # Adj. wild-captive " " "
+Grp.Corrs = Grp.Corrs[Sort.ix]
 
 # Display all significant with p < 0.05
 num_sig = sum(Wld.Qvals < 0.05, na.rm = T) # Count how many are significant
 C_ix = map$CaptiveWild=="Captive"          # Stores "true" if monkey is captive, else "false"
 W_ix = map$CaptiveWild=="Wild"             # As above. Use these to select just wild/captive monkeys
-sink(file = "Dumped_Significance.txt")     # Store output to a text file
-cat("Pathway\tGroupwise_Q\tCaptiveVsWild_Q\tTrendInCaptivity\n")  # The header line in the file
-if (num_sig) for (i in 1:num_sig) {        # Loop through all significant pathways
+sink(file = "PICRUSt_Significance.txt")    # Store output to a text file
+cat("Pathway\tPolyserial_Q\tPolyserial_Cor\tCaptiveVsWild_Q\tTrendInCaptivity\n")  # The header line in the file
+if (num_sig) for (i in 1:num_sig) {
   upInCaptive = mean(picrust[PicrustIDs[i],C_ix]) > mean(picrust[PicrustIDs[i],W_ix]) # compare avgs
-  cat(PicrustIDs[i],'\t',Grp.Qvals[i],'\t',Wld.Qvals[i],'\t',ifelse(upInCaptive,"UP","DOWN"),'\n')
+  cat(PicrustIDs[i],'\t',Grp.Qvals[i],'\t',-Grp.Corrs[i],'\t',Wld.Qvals[i],'\t',ifelse(upInCaptive,"UP","DOWN"),'\n',sep='')
 }
 sink(NULL)                                 # Close the output file
 
@@ -49,12 +58,12 @@ sink(NULL)                                 # Close the output file
 par(oma=c(0,2,0,0))                                  # Margins!
 myData = c(1,15,43,57)                               # Data!
 myLabels = c("USA zoo","SE Asia zoo","EPRC","Wild")  # Labels!
-myplot = barplot(myData, names=myLabels, ylim = c(0,62), xlab = "Population", ylab = "", col=0)
+myplot = barplot(myData, names=myLabels, ylim = c(0,62), xlab = "Population", ylab = "")
 mtext("Dietary Biodiversity\n(Number of plant species)",side=2,line=3) # Special Y axis!
 text(myplot,myData,myData, pos=3)                    # Add numbers on top of the bars!
 
 #### F/B Ratio ####
-otu = read.delim('EMP_seqs_R1_Doucs_otu_table_mc2_w_tax_open_ref_NO_CHLOROPLASTS.txt',row=1,skip=1)
+otu = read.delim('EMP_seqs_R1_Doucs_otu_table_mc2_w_tax_open_ref_NO_CHLOROPLASTS.txt',row=1,skip=1,as.is=T)
 otu = otu[,c(rownames(map),"taxonomy")]  # sync the sample names for the OTU table
 isFirmicutes = grepl('p__Firmicutes',otu$taxonomy) # Save "trues" where Firmicutes, false otherwise
 isBacteroides = grepl('p__Bacteroidetes',otu$taxonomy) # Like above
@@ -64,7 +73,7 @@ kruskal.test(FBratio ~ map$PA)
 for (i in 1:length(levels(map$PA))) for (j in i:length(levels(map$PA))) {
   if (i == j) next
   p = wilcox.test(FBratio[map$PA==levels(map$PA)[i]],FBratio[map$PA==levels(map$PA)[j]])
-  cat(levels(map$PA)[i]," vs ",levels(map$PA)[j]," p = ",p$p.value,"\n")
+  cat(levels(map$PA)[i]," vs ",levels(map$PA)[j]," p = ",p$p.value,"\n",sep='')
 }
 plot(FBratio ~ map$PA, xlab="Population", ylab="Log F:B ratio")
 df = data.frame(FBratio, map$PA)     # Split manually into groups
@@ -82,3 +91,62 @@ pcoa.w = plot_unifrac(otu.s,map,tree,category = 'Population',weighted=T)
 adonis(pcoa.u ~ map$Population)      # Do stats for clustering (unweighted)
 adonis(pcoa.w ~ map$Population)      # Do stats for clustering (weighted)
 adonis(bray ~ map$Population)        # Do stats for bray-curtis too, why not
+
+# do a boxplot of the main axis
+plot_pcoa(pcoa.u,map,category='Population')
+dists = pcoa(pcoa.u)$vectors
+boxplot(dists[,1] ~ map$Population, horizontal=T, las=2, ylim=c(-0.18,0.42),
+        col=c("#F8766D","#7CAE00","#00BFC4","#C77CFF"))
+
+#### Differential taxa testing ####
+# Massage the taxa names
+for (i in 1:7) otu$taxonomy = gsub("; .__$","",otu$taxonomy, perl=T)
+
+split = strsplit(otu$taxonomy,"; ")                 # Split by semicolon into levels
+taxaStrings = sapply(split,function(x) paste(x[1:6],collapse=";"))
+otu.t = rowsum(otu[,-ncol(otu)],taxaStrings)         # Collapse at desired level
+otu.t = sweep(otu.t,2,colSums(otu.t),'/');           # Normalize to relative abundance
+otu.t = otu.t[rowMeans(otu.t) >= 0.0001,];           # Drop rare taxa (abundance)
+otu.t = otu.t[rowSums(otu.t > 0) >= 3,];              # Drop rare taxa (prevalence)
+otu.t = sweep(otu.t,2,colSums(otu.t),'/');           # Re-normalize to relative abundance
+otu.t = otu.t[order(rowMeans(otu.t),decreasing=T),]; # Sort by avg. abundance
+
+# Go through each taxon and test for significance w/group
+#install.packages("polycor")
+library(polycor)
+otu.t = as.matrix(otu.t)
+CW = map$CaptiveWild %in% c("Captive","Wild") # For selecting only captive and wild
+Grp.Pvals = rep(1,nrow(otu.t)) # Initialize p values for groupwise sig. tests
+Grp.Corrs = rep(0,nrow(otu.t)) # Initialize correlation chamber (groupwise)
+Wld.Pvals = rep(1,nrow(otu.t)) # Initialize p values for wild vs captive tests
+for (m.ix in 1:nrow(otu.t)) {  # Loop through all the rows (taxa)
+  try({
+    ps = polyserial(otu.t[m.ix,],map$PA,ML=T,std.err = T)
+    Grp.Corrs[m.ix] = ps$rho
+    Grp.Pvals[m.ix] = 1-pchisq(ps$chisq, ps$df) 
+  },silent=T)
+  Wld.Pvals[m.ix] = wilcox.test(otu.t[m.ix,CW] ~ map$CaptiveWild[CW])$p.value
+}
+
+# Sort by significance, adjust for multiple tests
+Sort.ix = order(Grp.Pvals)                 # Stores the order of them (like third, first, second...)
+taxIDs = rownames(otu.t)[Sort.ix]          # Keep a list of the IDs in order
+Grp.Qvals = p.adjust(Grp.Pvals[Sort.ix],method = "holm") # Adj. group P-values in order
+Wld.Qvals = p.adjust(Wld.Pvals[Sort.ix],method = "holm") # Adj. wild-captive " " "
+Grp.Corrs = Grp.Corrs[Sort.ix]
+
+# Display all significant with p < 0.05
+num_sig = sum(Grp.Qvals < .05, na.rm = T) # Count how many are significant
+C_ix = map$CaptiveWild=="Captive"          # Stores "true" if monkey is captive, else "false"
+W_ix = map$CaptiveWild=="Wild"             # As above. Use these to select just wild/captive monkeys
+sink(file = "taxa_Significance.txt")                 # Get ready to write the significant ones
+cat("Taxon\tPolyserial_Q\tPolyserial_Cor\tCaptiveVsWild_Q\tTrendInCaptivity\n")  # The header line in the file
+pdf("TaxaSwarms.pdf",width = 8,height=7)
+if (num_sig) for (i in 1:num_sig) {
+  upInCaptive = mean(otu.t[taxIDs[i],C_ix]) > mean(otu.t[taxIDs[i],W_ix]) # compare avgs
+  cat(taxIDs[i],'\t',Grp.Qvals[i],'\t',-Grp.Corrs[i],'\t',Wld.Qvals[i],'\t',ifelse(upInCaptive,"UP","DOWN"),'\n',sep='')
+  beeswarm(otu.t[taxIDs[i],] ~ map$PA, col=c("#F8766D","#7CAE00","#00BFC4","#C77CFF"), # library(beeswarm)
+           xlab="Population",ylab="Relative Abundance",main=taxIDs[i],cex.axis=1.1,cex.main=0.75,corral="random")
+}
+dev.off()
+sink(NULL)
